76e5edcf8693f9f7a1e6a9cd6040f3b8af1600e1
BillyWalsh112 <20107248@mail.wit.ie>
2025-10-19 00:22:36 +0100

    Add task functionality

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
 import AddTaskForm from './components/Form';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [taskState, setTaskState] = useState({
@@ -10,6 +11,12 @@ function App() {
       { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium", done: false },
       { id: 3, title: "Tidy up", description: "Clean up the living room", deadline: "Today", priority: "Low", done: false }
     ]
+  });
+
+  const [formState, setFormState] = useState({
+    title: "",
+    description: "",
+    deadline: ""
   });
 
   const doneHandler = (taskIndex) => {
@@ -22,6 +29,41 @@ function App() {
     const tasks = [...taskState.tasks];  
     tasks.splice(taskIndex, 1);          
     setTaskState({ tasks });             
+  };
+
+  const formChangeHandler = (event) => {
+    let form = {...formState};
+
+    switch(event.target.name) {
+      case "title":
+        form.title = event.target.value;
+        break;
+      case "description":
+        form.description = event.target.value;
+        break;
+      case "deadline":
+        form.deadline = event.target.value;
+        break;
+      default:
+        form = formState;
+    }
+
+    setFormState(form);
+    console.log(formState);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const tasks = [...taskState.tasks];
+    const form = {...formState};
+
+    form.id = uuidv4();
+    form.priority = "Low";
+    form.done = false;
+
+    tasks.push(form);
+    setTaskState({ tasks });
   };
 
   return (
@@ -39,7 +81,7 @@ function App() {
           deleteTask={() => deleteHandler(index)} 
         />
       ))}
-      <AddTaskForm />
+      <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
     </div>
   );
 }
